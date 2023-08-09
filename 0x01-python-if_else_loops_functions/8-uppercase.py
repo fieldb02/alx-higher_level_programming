43a47a0da3795a82546dca9dbd426f1bb2099708
field02 <field02>
2023-08-09 19:28:11 +0300

    Finally

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    output = ""
+    
+    for n in str:
+        if n in 'abcdefghijklmnopqrstuvwxyz':
+            a = ord(n)
+            b = a - 32
+            b = chr(b)
+            output += b
+        else:
+            output += n
+    print("{}".format(output), end="")
+    print()
