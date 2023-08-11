f80863d1ad4c6945e787aa80782fc4760de137eb
field02 <field02>
2023-08-12 02:42:08 +0300

    Infinite loop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
     number = len(sys.argv) - 1
-    if number == 0:
-        print("{} arguments:".format(number))
     if number == 1:
-        print("{} argument.".format(number))
+        print("{} argument:".format(number))
+    if number == 0:
+        print("{} arguments.".format(number))
     else:
         print("{} arguments:".format(number))
     for i, a in enumerate(sys.argv[1:]):
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    numbers = len(sys.argv)
+    add = 0
+    for i in range(1, numbers):
+        add += int(sys.argv[i])
+    print("{}".format(add))
