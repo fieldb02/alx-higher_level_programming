810bfe8116936e1bc24f2194b5faff4e1de66801
field02 <field02>
2023-08-11 17:14:45 +0300

    toolbox fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    
     from calculator_1 import add
     a = 10
     b = 5
     addition = add(a, b)
-    print ("{} + {} = {}".format(a, b, addition))
-
+    print(a, "+", b, "=", addition)
+    
     from calculator_1 import sub
     a = 10
     b = 5
     subtract = sub(a, b)
-    print("{} - {} = {}".format(a, b, subtract))
-
+    print(a, "-", b, "=", subtract)
+    
     from calculator_1 import mul
     a = 10
     b = 5
     multiply = mul(a, b)
-    print ("{} * {} = {}".format(a, b, multiply))
+    print(a, "*", b, "=", multiply)
 
     from calculator_1 import div
     a = 10
     b = 5
     division = div(a, b)
-    print("{} / {} = {}".format(a, b, division))
+    print(a, "/", b, "=", division)
