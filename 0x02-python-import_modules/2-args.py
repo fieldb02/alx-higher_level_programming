5b22ca1ea4c295a22dba1eedad01cf73b90ea948
field02 <field02>
2023-08-12 02:33:19 +0300

    Argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     number = len(sys.argv) - 1
-    if number == 1:
-        print("{} argument:".format(number))
     if number == 0:
-        print("{} arguments.".format(number))
+        print("{} arguments:".format(number))
+    if number == 1:
+        print("{} argument.".format(number))
     else:
         print("{} arguments:".format(number))
     for i, a in enumerate(sys.argv[1:]):
