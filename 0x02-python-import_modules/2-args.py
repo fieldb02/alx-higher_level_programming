18d25aee46e1df8eae708ea4c67686d4afca64fd
field02 <field02>
2023-08-12 02:30:57 +0300

    Argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    number = len(sys.argv) - 1
+    if number == 1:
+        print("{} argument:".format(number))
+    if number == 0:
+        print("{} arguments.".format(number))
+    else:
+        print("{} arguments:".format(number))
+    for i, a in enumerate(sys.argv[1:]):
+        print("{}: {}".format(i + 1, a))
