89aa379d5621109d9d0abc0f5731c0581fd374dc
field02 <field02>
2023-08-29 01:09:28 +0300

    safe print 3

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    for b in range(x):
+        try:
+            print("{:d}".format(my_list[b]), end="")
+            k += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return (k)
+
