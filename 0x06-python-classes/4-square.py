d573fdfa783bb8eb45a838c177645ab39e50bf02
field02 <field02>
2023-08-29 21:54:22 +0300

    classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Square Class Definition"""
+
+
+class Square:
+    """Representation of a square geometry.
+
+    Attributes:
+        side_length (int): Length of the square's side
+
+    """
+    def __init__(self, side_length=0):
+        """Initialize a Square instance.
+
+        Args:
+            side_length (int, optional): Length of square's side
+
+        Raises:
+             TypeError: if side_length is not an integer
+             ValueError: If side_length is less than zero
+
+        """
+        self.side_length = side_length
+
+    @property
+    def side_length(self):
+        """Get the side length property.
+
+        Returns:
+            int: side_length
+        """
+        return self.__side_length
+
+    @side_length.setter
+    def side_length(self, value):
+        """Set the side length property."""
+
+        if not isinstance(value, int):
+            raise TypeError("side_length must be an integer")
+        elif value < 0:
+            raise ValueError("side_length must be >= 0")
+        else:
+            self.__side_length = value
+
+    def calculate_area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square
+
+        """
+        return int(self.side_length) ** 2
