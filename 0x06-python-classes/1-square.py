7dd441910ffaf97460d650564bdff80645f76203
field02 <field02>
2023-08-29 21:43:51 +0300

    class that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defining a Square class"""
+
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        side_length (int): Length of the square's side
+
+    """
+
+    def __init__(self, side_length):
+        """Initialize a Square instance.
+
+        Args:
+            side_length (int): Length of the square's side
+
+        """
+        self.__side_length = side_length
