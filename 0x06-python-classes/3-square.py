7d217ad8af018c380eb312cfd0dd5232d96c43f5
field02 <field02>
2023-08-29 23:12:06 +0300

    Error fixed

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,38 +1,21 @@
 #!/usr/bin/python3
-"""Square Class Definition"""
+"""Square class definition"""
 
 
 class Square:
-    """Representation of a square geometry.
+    """Represention of a square."""
 
-    Attributes:
-        side_length (int): Length of the square's side
-
-    """
-
-    def __init__(self, side_length=0):
-        """Initialize a Square instance.
-
+    def __init__(self, size=0):
+        """Initialize a square instance.
         Args:
-            side_length (int): Length of the square's side
-
-        Raises:
-            TypeError: If side_length is not an integer
-            ValueError: If side_length is less than zero
-
+            size (int): Length of square size.
         """
-        if not isinstance(side_length, int):
-            raise TypeError("side_length must be an integer")
-        elif side_length < 0:
-            raise ValueError("side_length must be >= 0")
-        else:
-            self.__side_length = side_length
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-    def calculate_area(self):
-        """Calculate the area of the square.
-
-        Returns:
-            int: The area of the square
-
-        """
-        return self.__side_length ** 2
+    def area(self):
+        """Calculate area of square."""
+        return (self.__size * self.__size)
