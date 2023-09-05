86a10d38d2742f6a4a9405417193b5a201e2cfee
field02 <field02>
2023-09-05 22:45:49 +0300

    More Classes and Objects

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+'''Def a rectangle'''
+
+
+class Rectangle:
+    '''Classing a regtangle'''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    '''attribute:
+        width(int): width of rectangle
+        height(int): heightof rectangle
+    '''
+    '''retrieving width'''
+    @property
+    def width(self):
+        return self.__width
+
+    '''setting width'''
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    '''retrieving height'''
+    @property
+    def height(self):
+        return self.__height
+
+    '''setting height'''
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    '''area of rectangle'''
+    def area(self):
+        return self.__width * self.__height
+
+    '''perimeter of rectangle'''
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    '''print area as #'''
+    def pr_st(self):
+        if self.__width == 0:
+            return ''
+        else:
+            return '\n'.join(
+                str('#') * self.__width for i in range(self.__height))
+
+    '''return area as a str(#)'''
+    def __str__(self):
+        return f"{self.pr_st()}"
+    def __repr__(self):
+        return f"Rectangle{self.__width, self.__height}"
+    def __del__(self):
+        print("Bye rectangle...")
