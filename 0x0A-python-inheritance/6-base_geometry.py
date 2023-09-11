b7f855bad48c25b6b575e767835e0e0c6ae5fcf7
field02 <field02>
2023-09-11 22:48:05 +0300

    Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """empty geomtry module"""
+    def area(self):
+        """Shows the error"""
+        raise Exception("area() is not implemented")
